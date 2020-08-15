58c78d6a4a23dd3b2d615a8c5a6eefb8d30c7628
Gerardo Paredes <gerardparedes23@gmail.com>
2020-08-15 12:30:19 -0700

    Query added to Post.ts

--- api/graphql/Post.ts
+++ api/graphql/Post.ts
@@ -9,4 +9,19 @@ schema.objectType({
         t.string('body') // body of type 'string'
         t.boolean('published') // published of type 'boolean'
     },
-});
+})
+
+// Post Query 
+schema.extendType({
+    type: 'Query',
+    definition(t) {
+        t.field('drafts', { // field name
+            nullable: false, // clients will always get a value for this field
+            type: 'Post', // field type
+            list: true, // [Post] <- list type 
+            resolve() {
+                return [{ id: 1, title: 'Nexus', body: '...', published: false }]
+            }
+        })
+    },
+})
