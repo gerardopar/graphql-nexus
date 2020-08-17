00831d88c5ffe7fbc7719045840321f131aa595c
Gerardo Paredes <gerardparedes23@gmail.com>
2020-08-17 10:15:18 -0700

    prisma-nexus plugin added

--- api/app.ts
+++ api/app.ts
@@ -1,5 +1,9 @@
 import { schema } from 'nexus';
 import { db } from './db';
+import { use } from 'nexus';
+import { prisma } from 'nexus-plugin-prisma';
+
+use(prisma());
 
 schema.addToContext(() => {
     return {
